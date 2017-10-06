79fc47a395669616e14d5c01bd32328e0b6461e8
nguyen18 <31105362+nguyen18@users.noreply.github.com>
2017-10-05 23:08:37 -0700

    yeet

--- ProcessingNumbers/src/ProcessingNumbers.java
+++ ProcessingNumbers/src/ProcessingNumbers.java
@@ -5,159 +5,55 @@
 import java.util.Scanner;
 public class ProcessingNumbers {
 
-	private static Scanner nums;
+	private static Scanner scans;
 
 	public static void main(String[] args) {
-		System.out.println("enter 4 numbers. This programs finds the largest and smallest integer from numbers inputed");
-		nums = new Scanner(System.in);
-		System.out.println("enter first integer");
-		int num1 = nums.nextInt();                    
-		System.out.println("enter second integer");
-		int num2 = nums.nextInt();
-		System.out.println("enter third integer");
-		int num3 = nums.nextInt();
-		System.out.println("enter last integer");
-		int num4 = nums.nextInt();
+		scans = new Scanner(System.in);
+		int input = 0;
+		int max = 0;
+		int min = 0;
+		int sumEven = 0;
+		int maxEven = 0;
+		boolean done = false;
 		
-// these if loops return the largest integer
-		if(num1 >= num2) {
-			
-			if(num1 >= num3) {
-				
-				if(num1 >= num4) {
-					
-					System.out.println("Largest integer: " + num1);
-					
-				} else {
-					
-					System.out.println("Largest integer: " + num4);
-				}
-			} else if (num3 >= num4) {
-				
-				System.out.println("Largest integer: " + num3);
-				
-			} else {
-				
-				System.out.println("Largest integer: " + num4);
+		System.out.println("This program finds the max integer, smallest integer, sum of the even numbers and the max even number from numbers inputed");
+		do {
+			System.out.println("Input Integer:");
+			input = scans.nextInt();
+			if(max <= input) {
+				max = input;
 			} 
 			
-		} else if( num2 >= num3) {
-				
-				if(num2 >= num4) {
-					
-
-					System.out.println("Largest integer: " + num2);
-					
-				}else { System.out.println("Largest integer: " + num4);
-				}
-			} else if (num3 >= num4) { 
-				System.out.println("Largest integer:"  + num3);
-				
-			} else {
-				System.out.println("Largest integer: " + num4);
+			if(max >= input || min >= input) {
+				min = input;
 			}
-//these if loops return the smallest integer
-		if(num1 <= num2) {
-			
-			if(num1 <= num3) {
-				
-				if(num1 <= num4) {
-					
-					System.out.println("Smallest integer: " + num1);
-					
-				} else {
-					
-					System.out.println("Smallest integer: " + num4);
+			if(input % 2 == 0) {
+				if(maxEven <= input) {
+					maxEven = input;
 				}
-			} else if (num3 <= num4) {
+				sumEven += input;
+			}
+			System.out.println("Done? yes or no");
+			String repeat = scans.next();
+					while(!repeat.equals("yes") && !repeat.equals("no")){ 
+						System.out.println("error. please type yes or no");
+						repeat = scans.next();
+					} 
 				
-				System.out.println("Smallest integer: " + num3);
-				
-			} else {
-				
-				System.out.println("Smallest integer: " + num4);
-			} 
-			
-		} else if( num2 <= num3) {
-				
-				if(num2 <= num4) {
-					
-
-					System.out.println("Smallest integer: " + num2);
-					
-				}else { System.out.println("Smallest integer: " + num4);
+				if(repeat.equals("no")) {
+					done = false;
+				} else if(repeat.equals("yes")) {
+					done = true;
 				}
-		} else if (num3 <= num4) { 
-			System.out.println("Smallest integer:"  + num3);
+		} while (!done);
+			System.out.println("Largest Integer: " + max);
+			System.out.println("Smallest Integer: " + min);
+			System.out.println("Largest even integer: " + maxEven);
+			System.out.println("Sum of even numbers: " + sumEven);
 			
-		} else {
-			System.out.println("Smallest integer: " + num4);
 		}
-// finds all the even numbers
-		System.out.print("Even numbers: " );
-		boolean num1Even = false;
-		boolean num2Even = false;
-		boolean num3Even = false;
-		boolean num4Even = false;
-		if(num1 % 2 == 0) {
-			num1Even = true;
-			System.out.print(num1 + ", ");
-		}
-		if(num2 % 2 == 0) {
-			num2Even = true;
-			System.out.print(num2 + ", ");
-		}
-		if(num3 % 2 == 0) {
-			num3Even = true;
-			System.out.print(num3 + ", ");
-		}
-		if(num4 % 2 == 0) {
-			num4Even = true;
-			System.out.print(num4 + ", ");
-		}
-		System.out.println();
-// prints largest even number
-		System.out.print("If all 4 numbers are even, program will print largest even number: ");
-		if(num1Even == true && num2Even == true && num3Even == true && num4Even == true) {
-			if(num1 >= num2) {
-				
-				if(num1 >= num3) {
-					
-					if(num1 >= num4) {
-						
-						System.out.println(num1);
-						
-					} else {
-						
-						System.out.println(num4);
-					}
-				} else if (num3 >= num4) {
-					
-					System.out.println(num3);
-					
-				} else {
-					
-					System.out.println(num4);
-				} 
-				
-			} else if( num2 >= num3) {
-					
-					if(num2 >= num4) {
-						
-
-						System.out.println(num2);
-						
-					}else { System.out.println(num4);
-					}
-				} else if (num3 >= num4) { 
-					System.out.println(num3);
-				
-				} else {
-					System.out.println(num4);
-				}
-		}
-
 	}
-}
+	
+
 				
 
